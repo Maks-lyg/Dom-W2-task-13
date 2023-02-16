cb4f8871217e4511c37e32c8c4f37e3c88544d4b
Maks <maks-lug333@yandex.ru>
2023-02-17 07:44:49 +0900

    OK

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int num = 0;
+if (x > 100)
+{
+    num = (x - (x / 100 * 100)) % 10;
+    Console.WriteLine($"Третье число = {num}");
+}
+else
+Console.WriteLine("Третьей цифры нет");
